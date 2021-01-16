5587465da95e8d16997663ba8937cca64f638f3b
Chris Nappi <xnappo@jupiter.localdomain>
2021-01-16 11:55:12 -0600

    Ever so slightly less hacky Kodi service

--- service.py
+++ service.py
@@ -6,34 +6,31 @@ class XBMCPlayer( xbmc.Player ):
 
     def onPlayBackStarted( self ):
         # Will be called when xbmc starts playing a file
+        # Define services and app names, add the _S just in case you have a show/movie called 'Amazon' or something
+        services = {
+            'Amazon_S':'com.amazon.amazonvideo.livingroom', 
+            'Netflix_S':'com.netflix.ninja', 
+            'HBO_S':'com.hbo.hbonow', 
+            'Discovery_S':'com.discovery.discoveryplus.androidtv'
+        }
+
         xbmc.log( "Testing name for Netflix/Amazon/HBOMax dummy match: " + str(self.getPlayingFile(self)) )
-        intent   = ''
-        dataType = ''
-        dataURI  = ''
-        if "Netflix_S" in str(self.getPlayingFile(self)):
-            xbmc.sleep(400)
-            while(self.isPlaying() == True):
-                self.seekTime(10)
-	        xbmc.sleep(200)
-            app      = 'com.netflix.ninja'
-            cmd = 'StartAndroidActivity("%s", "%s", "%s", "%s")' % (app, intent, dataType, dataURI)
-            xbmc.executebuiltin(cmd)
-        elif "Amazon_S" in str(self.getPlayingFile(self)):
-            xbmc.sleep(400)
-            while(self.isPlaying() == True):
-                self.seekTime(10)
-            	xbmc.sleep(200)
-            app      = 'com.amazon.amazonvideo.livingroom'
-            cmd = 'StartAndroidActivity("%s", "%s", "%s", "%s")' % (app, intent, dataType, dataURI)
-            xbmc.executebuiltin(cmd)
-        elif "HBO Max_S" in str(self.getPlayingFile(self)) or "HBO_S" in str(self.getPlayingFile(self)):
-            xbmc.sleep(400)
-            while(self.isPlaying() == True):
-                self.seekTime(10)
+        # Go through our list of streaming services
+        for service in services:
+            # Check if the playing file matches a streaming service
+            if service in str(self.getPlayingFile(self)):
+                # This bit below is to ensure the show gets marked watched
+                xbmc.sleep(400)
+                while(self.isPlaying() == True):
+                    self.seekTime(10)
                 xbmc.sleep(200)
-            app      = 'com.hbo.hbonow'
-            cmd = 'StartAndroidActivity("%s", "%s", "%s", "%s")' % (app, intent, dataType, dataURI)
-            xbmc.executebuiltin(cmd)
+
+                # This command's argument are app, intent, dataType and dataURI, but currently
+                # no way to make use of the intent etc to go to a particular show. (please let me know!)
+                cmd = 'StartAndroidActivity("%s", "%s", "%s", "%s")' % (services[service], '', '', '')
+                
+                # Run the command
+                xbmc.executebuiltin(cmd)
 
 player = XBMCPlayer()
 
