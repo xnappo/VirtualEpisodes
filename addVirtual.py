aecdf7d16664b08b1bb50dc231834916f722abf3
Chris Nappi <xnappo@jupiter.localdomain>
2021-01-17 11:03:12 -0600

    Lint for the PC side files

--- addVirtual.py
+++ addVirtual.py
@@ -9,8 +9,10 @@ from shutil import copyfile
 with open("config.yaml") as config:
     configData = yaml.load(config, Loader=yaml.FullLoader)
 
-url = "http://" + configData['host']+ "/api/series/?apikey=" + configData['apiKey']	
-try: sys.argv[1]
+url = "http://" + configData['host'] + \
+    "/api/series/?apikey=" + configData['apiKey']
+try:
+    sys.argv[1]
 except:
     print ('Usage: addVirtual.py <partial title of show to add>')
     sys.exit()
@@ -27,18 +29,22 @@ for item in jsonResponse:
 
         if network in configData['networks']:
             if not path.exists(configData['basePath'] + "/" + item['title']):
-                os.mkdir(configData['basePath'] + "/" + item['title'])            
-            for season in range (1, item['seasonCount']+1):
+                os.mkdir(configData['basePath'] + "/" + item['title'])
+            for season in range(1, item['seasonCount']+1):
                 showSeason = "{:02d}".format(season)
                 if not path.exists(configData['basePath'] + "/" + item['title'] + "/Season " + showSeason):
-                    os.mkdir(configData['basePath'] + "/" + item['title'] + "/Season " + showSeason)                
-                for episode in range (1, item['seasons'][season-1]['statistics']['totalEpisodeCount']+1):
+                    os.mkdir(configData['basePath'] + "/" +
+                             item['title'] + "/Season " + showSeason)
+                for episode in range(1, item['seasons'][season-1]['statistics']['totalEpisodeCount']+1):
                     showEpisode = "{:02d}".format(episode)
-                    print ("Adding: " + item['title'] + " " + "S" + showSeason + "E" + showEpisode + " from " + network)
+                    print ("Adding: " + item['title'] + " " + "S" +
+                           showSeason + "E" + showEpisode + " from " + network)
                     if not path.exists(configData['basePath'] + "/" + item['title'] + "/Season " + showSeason + "/" + network + "_S" + showSeason + "E" + showEpisode + ".mp4"):
-                        copyfile(configData['dummyFile'], configData['basePath'] + "/" + item['title'] + "/Season " + showSeason + "/" + network + "_S" + showSeason + "E" + showEpisode + ".mp4")
+                        copyfile(configData['dummyFile'], configData['basePath'] + "/" + item['title'] +
+                                 "/Season " + showSeason + "/" + network + "_S" + showSeason + "E" + showEpisode + ".mp4")
 if found == False:
-    print ('Series: ' + sys.argv[1] + ' not found!  Please use addVirtual.py with one of the following series:\n')
+    print ('Series: ' +
+           sys.argv[1] + ' not found!  Please use addVirtual.py with one of the following series:\n')
     for item in jsonResponse:
         if item['network'] in configData['networkMaps']:
             network = configData['networkMaps'][item['network']]
@@ -47,4 +53,3 @@ if found == False:
 
         if network in configData['networks']:
             print (item['title'] + " [" + network + "]")
-
--- autoVirtual.py
+++ autoVirtual.py
@@ -8,16 +8,19 @@ from datetime import date
 from datetime import timedelta
 
 # Modify below for debug
-daysBack = 0 # Used for debug to get more episodes
-daysAhead = 1 # Used for debug to get more episodes, one is actually for current day.
-startDate = date.today() - timedelta(daysBack) 
+daysBack = 0  # Used for debug to get more episodes
+# Used for debug to get more episodes, one is actually for current day.
+daysAhead = 1
+startDate = date.today() - timedelta(daysBack)
 endDate = date.today() + timedelta(daysAhead)
 # End of debug section
 
 with open("config.yaml") as config:
     configData = yaml.load(config, Loader=yaml.FullLoader)
 
-url = "http://" + configData['host'] + "/api/calendar/?apikey=" + configData['apiKey'] + "&unmonitored=true&start=" + startDate.strftime("%Y-%m-%d") + "&end=" + endDate.strftime("%Y-%m-%d")
+url = "http://" + configData['host'] + "/api/calendar/?apikey=" + configData['apiKey'] + \
+    "&unmonitored=true&start=" + \
+    startDate.strftime("%Y-%m-%d") + "&end=" + endDate.strftime("%Y-%m-%d")
 
 jsonResponse = requests.get(url).json()
 for item in jsonResponse:
@@ -29,11 +32,14 @@ for item in jsonResponse:
     if network in configData['networks']:
         showSeason = "{:02d}".format(item['seasonNumber'])
         showEpisode = "{:02d}".format(item['episodeNumber'])
-        print ("Adding: " + item['series']['title'] + " " + "S" + showSeason + "E" + showEpisode + " from " + network)
+        print ("Adding: " + item['series']['title'] + " " + "S" +
+               showSeason + "E" + showEpisode + " from " + network)
         # Make directories/files as needed
         if not path.exists(configData['basePath'] + "/" + item['series']['title']):
             os.mkdir(configData['basePath'] + "/" + item['series']['title'])
         if not path.exists(configData['basePath'] + "/" + item['series']['title'] + "/Season " + showSeason):
-            os.mkdir(configData['basePath'] + "/" + item['series']['title'] + "/Season " + showSeason)
+            os.mkdir(configData['basePath'] + "/" +
+                     item['series']['title'] + "/Season " + showSeason)
         if not path.exists(configData['basePath'] + "/" + item['series']['title'] + "/Season " + showSeason + "/" + network + "_S" + showSeason + "E" + showEpisode + ".mp4"):
-            copyfile(configData['dummyFile'], configData['basePath'] + "/" + item['series']['title'] + "/Season " + showSeason + "/" + network + "_S" + showSeason + "E" + showEpisode + ".mp4")
+            copyfile(configData['dummyFile'], configData['basePath'] + "/" + item['series']['title'] +
+                     "/Season " + showSeason + "/" + network + "_S" + showSeason + "E" + showEpisode + ".mp4")
