28d566fae5d7d2721df42f2d5969e2a2faa7914a
Chris Nappi <xnappo@jupiter.localdomain>
2021-02-28 13:19:53 -0600

    Updated for Matrix, making addon a bit more real

--- service.py
+++ service.py
@@ -1,22 +1,20 @@
 import xbmc
 
-
-class XBMCPlayer(xbmc.Player):
-    def __init__(self, *args):
+class XBMCPlayer( xbmc.Player ):
+    def __init__( self, *args ):
         pass
 
-    def onPlayBackStarted(self):
+    def onPlayBackStarted( self ):
         # Will be called when xbmc starts playing a file
         # Define services and app names, add the _S just in case you have a show/movie called 'Amazon' or something
         services = {
-            'Amazon_S': 'com.amazon.amazonvideo.livingroom',
-            'Netflix_S': 'com.netflix.ninja',
-            'HBO_S': 'com.hbo.hbonow',
-            'Discovery_S': 'com.discovery.discoveryplus.androidtv'
+            'Amazon_S':'com.amazon.amazonvideo.livingroom', 
+            'Netflix_S':'com.netflix.ninja', 
+            'HBO_S':'com.hbo.hbonow', 
+            'Discovery_S':'com.discovery.discoveryplus.androidtv'
         }
 
-        xbmc.log("Testing name for Netflix/Amazon/HBOMax dummy match: " +
-                 str(self.getPlayingFile(self)))
+        xbmc.log( "Testing name for Netflix/Amazon/HBOMax dummy match: " + str(self.getPlayingFile(self)) )
         # Go through our list of streaming services
         for service in services:
             # Check if the playing file matches a streaming service
@@ -29,14 +27,13 @@ class XBMCPlayer(xbmc.Player):
 
                 # This command's argument are app, intent, dataType and dataURI, but currently
                 # no way to make use of the intent etc to go to a particular show. (please let me know!)
-                cmd = 'StartAndroidActivity("%s", "%s", "%s", "%s")' % (
-                    services[service], '', '', '')
-
+                cmd = 'StartAndroidActivity("%s", "%s", "%s", "%s")' % (services[service], '', '', '')
+                
                 # Run the command
                 xbmc.executebuiltin(cmd)
 
-
 player = XBMCPlayer()
-
-while not xbmc.abortRequested:
-    xbmc.sleep(1000)
+monitor = xbmc.Monitor()
+# wait for 10 seconds for the Kodi splash screen to close
+while not monitor.abortRequested():
+    xbmc.sleep(100)
